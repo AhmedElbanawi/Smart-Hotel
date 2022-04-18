0624415566e00797bccc24b9a068bf1ae1e0b61b
Ahmed Elbanawi <elbanawiahmed@gmail.com>
2022-04-18 03:54:18 -0700

    Add files via upload

--- GateWay/DISP.c
+++ GateWay/DISP.c
@@ -3,40 +3,7 @@ ECU_RM_DATA_t ECU_RM_Tmp_Data[MAX_ECU_RM];
 ECU_OD_DATA_t ECU_OD_Tmp_Data;
 void DISP_Update(void){
 	static char StrTemp[3];
-#if 0
-	static UINT8_t Cnt=0;
-	++Cnt;
-	if(10==Cnt){
-		Cnt = 0;
-		LCD_Jumb_To(ROW1,COL7);
-		LCD_Write_Char(' ');
-		LCD_Jumb_To(ROW1,COL6);
-		LCD_Disp_Number(ECU_OD_Data.Temp);
 
-		//LCD_Jumb_ToAndWrite("light: ", ROW2, COL1);
-		switch (ECU_OD_Data.Light) {
-		case ECU_OD_LIGHT_MORNING:
-			LCD_Jumb_ToAndWrite("M", ROW2, COL8);
-			break;
-		case ECU_OD_LIGHT_NIGHT:
-			LCD_Jumb_ToAndWrite("N", ROW2, COL8);
-			break;
-		default:
-			break;
-		}
-		if (ECU_RM_Data[0].SubLight == ECU_RM_SUB_LIGHT
-				&& ECU_RM_Data[0].SubTemp == ECU_RM_SUB_TMP) {
-			LCD_Jumb_ToAndWrite("TL", ROW1, COL15);
-		} else if (ECU_RM_Data[0].SubLight == ECU_RM_SUB_LIGHT) {
-			LCD_Jumb_ToAndWrite("L ", ROW1, COL15);
-		} else if (ECU_RM_Data[0].SubTemp == ECU_RM_SUB_TMP) {
-			LCD_Jumb_ToAndWrite("T ", ROW1, COL15);
-		} else {
-			LCD_Jumb_ToAndWrite("  ", ROW1, COL15);
-		}
-
-	}
-#endif
 	static UINT8_t State = 0;
 	switch (State) {
 	case 0:
@@ -72,6 +39,7 @@ void DISP_Update(void){
 		if(ECU_OD_CNT_OFF==ECU_OD_Tmp_Data.State)
 		{
 			ECU_OD_Tmp_Data.Temp=TMP_Back_Up;
+
 		}else if(ECU_OD_CNT_ON==ECU_OD_Tmp_Data.State)
 		{
 			ECU_OD_Tmp_Data.Temp=ECU_OD_Data.Temp;
